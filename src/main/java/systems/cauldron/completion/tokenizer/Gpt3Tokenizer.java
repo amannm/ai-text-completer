eafeadc1bcf68d9bdec2cc8ecd3be3ea2a4a46e7
Amann Malik <amannmalik@gmail.com>
2022-01-09 00:13:51 -0600

    cache tokenization results

--- src/main/java/systems/cauldron/completion/tokenizer/Gpt3Tokenizer.java
+++ src/main/java/systems/cauldron/completion/tokenizer/Gpt3Tokenizer.java
@@ -6,7 +6,9 @@ import systems.cauldron.completion.tokenizer.bpe.SymbolPair;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,9 +17,11 @@ public class Gpt3Tokenizer implements Tokenizer {
     private static final Pattern GPT3_PRETOKEN_PATTERN = Pattern.compile("'s|'t|'re|'ve|'m|'ll|'d| ?\\p{L}+| ?\\p{N}+| ?[^\\s\\p{L}\\p{N}]+|\\s+(?!\\S)|\\s+", Pattern.UNICODE_CHARACTER_CLASS);
 
     private final Gpt3BpeReader pairLookup;
+    private final Map<String, List<String>> tokenCache;
 
     public Gpt3Tokenizer() {
         this.pairLookup = new Gpt3BpeReader();
+        this.tokenCache = new ConcurrentHashMap<>();
     }
 
     public List<String> tokenize(String text) {
@@ -25,16 +29,19 @@ public class Gpt3Tokenizer implements Tokenizer {
         Matcher preTokenMatcher = GPT3_PRETOKEN_PATTERN.matcher(text);
         while (preTokenMatcher.find()) {
             String preToken = preTokenMatcher.group();
-            List<String> tokens = initializeTokens(preToken);
-            while (true) {
-                Set<SymbolPair> pairs = generatePairs(tokens);
-                SymbolPair minRankPair = pairLookup.selectMinRankPair(pairs);
-                if (minRankPair == null) {
-                    break;
+            List<String> cachedTokens = tokenCache.computeIfAbsent(preToken, x -> {
+                List<String> tokens = initializeTokens(preToken);
+                while (true) {
+                    Set<SymbolPair> pairs = generatePairs(tokens);
+                    SymbolPair minRankPair = pairLookup.selectMinRankPair(pairs);
+                    if (minRankPair == null) {
+                        break;
+                    }
+                    tokens = mergeTokensByPair(minRankPair, tokens);
                 }
-                tokens = mergeTokensByPair(minRankPair, tokens);
-            }
-            result.addAll(tokens);
+                return tokens;
+            });
+            result.addAll(cachedTokens);
         }
         return result;
     }
